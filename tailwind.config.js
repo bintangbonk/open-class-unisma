b26cb38a4714a053b0eb7d284dd0af570e41be62
bintangbonk <bintangbongkys@gmail.com>
2023-06-09 11:42:51 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}",
+  "./node_modules/tw-elements/dist/js/**/*.js"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      screens: {
+        "2xl": "1320px",
+      },},
+  },
+  plugins: [require('flowbite/plugin'),require("tw-elements/dist/plugin.cjs")],
+}
+
